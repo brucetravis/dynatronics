227e1d942790d60c4163a04f8054bb4b863117b0
Lum-ona <lumonamulengwa@gmail.com>
2025-08-17 17:46:55 +0300

    added background image and styled product item

--- src/App.js
+++ src/App.js
@@ -1,69 +1,67 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 // import Shop from './pages/shop/Shop'
-import Shop from './pages/shop/Shop'
-import Home from './pages/home/Home'
-import Register from './pages/register/Register'
-import 'react-toastify/dist/ReactToastify.css' // Don't forget the styles
-import { ToastContainer } from 'react-toastify';
-import BodyIcons from './components/common/bodyicons/BodyIcons'
-import CookieBanner from './components/cookies/CookieBanner';
-import ProductsUpload from './pages/productsupload/ProductsUpload';
-import ProductDetails from './pages/productdetails/ProductDetails';
-import ShoppingCart from './components/shoppingcart/ShoppingCart';
-import { AnimatePresence } from 'framer-motion';
-import WishList from './components/wishlist/WishList';
-import SearchBar from './components/searchbar/SearchBar';
-import { useShop } from './contexts/ShopProvider';
-import { useWish } from './contexts/WishListProvider';
-import { useSearch } from './contexts/SearchProvider';
-import Payment from './pages/payment/Payment';
-import About from './pages/about/About';
-import CategoryDetails from './pages/categorydetails/CategoryDetails';
+import Shop from "./pages/shop/Shop";
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import "react-toastify/dist/ReactToastify.css"; // Don't forget the styles
+import { ToastContainer } from "react-toastify";
+import BodyIcons from "./components/common/bodyicons/BodyIcons";
+import CookieBanner from "./components/cookies/CookieBanner";
+import ProductsUpload from "./pages/productsupload/ProductsUpload";
+import ProductDetails from "./pages/productdetails/ProductDetails";
+import ShoppingCart from "./components/shoppingcart/ShoppingCart";
+import { AnimatePresence } from "framer-motion";
+import WishList from "./components/wishlist/WishList";
+import SearchBar from "./components/searchbar/SearchBar";
+import { useShop } from "./contexts/ShopProvider";
+import { useWish } from "./contexts/WishListProvider";
+import { useSearch } from "./contexts/SearchProvider";
+import Payment from "./pages/payment/Payment";
+import About from "./pages/about/About";
+import CategoryDetails from "./pages/categorydetails/CategoryDetails";
 
 function App() {
-
-  // Get the shopping cart state from the ShopProvider in order to display the shopping cart when 
+  // Get the shopping cart state from the ShopProvider in order to display the shopping cart when
   // the shoppping cart icon is clicked
-  const { showCart } = useShop()
+  const { showCart } = useShop();
 
   // Get the WishList state from the WishListProvider to display the WishList
-  const { showWish } = useWish()
+  const { showWish } = useWish();
 
   // Get the Search bar state for the SearchProvider to display the searchBar
-  const { showSearch } = useSearch()
+  const { showSearch } = useSearch();
 
   return (
     <div className="App">
-      <Routes>
-        <Route path={'/'} element={<Shop />} />
-        <Route path={'/home'} element={<Home />} />
-        <Route path={'/about'} element={<About />} />
-        <Route path={'/register'} element={<Register />} />
-        <Route path={'/payment/:userId'} element={<Payment />} />
-        <Route path={'/productsupload'} element={<ProductsUpload />} />
-        <Route path='/productdetails/:id' element={<ProductDetails />} />
-        <Route path='/categorydetails/:categoryName' element={<CategoryDetails />} />
-      </Routes>
+      <div className="app-container">
+        <Routes>
+          <Route path={"/"} element={<Shop />} />
+          <Route path={"/home"} element={<Home />} />
+          <Route path={"/about"} element={<About />} />
+          <Route path={"/register"} element={<Register />} />
+          <Route path={"/payment/:userId"} element={<Payment />} />
+          <Route path={"/productsupload"} element={<ProductsUpload />} />
+          <Route path="/productdetails/:id" element={<ProductDetails />} />
+          <Route
+            path="/categorydetails/:categoryName"
+            element={<CategoryDetails />}
+          />
+        </Routes>
 
-      <AnimatePresence>
-        { showCart && (
-          <ShoppingCart key="shop" />
-        )}
+        <AnimatePresence>
+          {showCart && <ShoppingCart key="shop" />}
 
-        { showWish && (
-          <WishList key="wish" />
-        )}
+          {showWish && <WishList key="wish" />}
 
-        { showSearch && (
-          <SearchBar key="search" />
-        )}
-        
-        <CookieBanner key="cookie" />
-      </AnimatePresence>
-      
-      <BodyIcons />
-      <ToastContainer position="bottom-left" autoClose={3000} />
+          {showSearch && <SearchBar key="search" />}
+
+          <CookieBanner key="cookie" />
+        </AnimatePresence>
+
+        <BodyIcons />
+        <ToastContainer position="bottom-left" autoClose={3000} />
+      </div>
     </div>
   );
 }
